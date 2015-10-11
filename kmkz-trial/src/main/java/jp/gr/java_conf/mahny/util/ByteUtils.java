a814992a7000969b2c43cc41ce555c6d5b58922d
mahny_b <mahny.brand@gmail.com>
2015-10-11 15:01:12 +0900

    byte配列比較ユーティリティ追加

--- kmkz-trial/src/main/java/jp/gr/java_conf/mahny/util/ByteUtils.java
+++ kmkz-trial/src/main/java/jp/gr/java_conf/mahny/util/ByteUtils.java
@@ -6,6 +6,8 @@ package jp.gr.java_conf.mahny.util;
  */
 public class ByteUtils {
 
+	private static int NOT_FOUND = -1;
+
 	/**
 	 * @deprecated newしちゃダメ
 	 */
@@ -20,8 +22,62 @@ public class ByteUtils {
 	 * @param dest 部分配列
 	 * @param fromIndex 検索開始インデックス（負数は0と同じ）
 	 * @return　インデックス番号（見つからなかったら-1）
+	 * {@link http://capm-network.com/?tag=C%E8%A8%80%E8%AA%9E%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0-BM%E6%B3%95}
 	 */
 	public static int indexOf(byte[] org, byte[] dest, int fromIndex) {
-		return Integer.MIN_VALUE;
+		fromIndex = fromIndex < 0 ? 0 : fromIndex;
+
+		// 前提から死んでるω
+		if (isEmpty(org) || isEmpty(dest)) {
+			return NOT_FOUND;
+		}
+		if (org.length < dest.length + fromIndex) {
+			return NOT_FOUND;
+		}
+
+		// ずらし用のカンペ作成
+		// 扱うデータがbyte型なので、256個確保してしまう。
+		int[] skipTable = new int[Byte.MAX_VALUE - Byte.MIN_VALUE + 1];
+		for (int i = 0; i < skipTable.length; i++) {
+			skipTable[i] = dest.length;
+		}
+		for (int i = 0; i < dest.length; i++) {
+			int index = dest[i] - Byte.MIN_VALUE;
+			int skip = (dest.length - (i + 1) != 0) ? dest.length - (i + 1) : 1;
+			skipTable[index] = (skipTable[index] == dest.length) ? skip : dest.length;
+		}
+
+		// 探索位置情報
+		int beginIndex = fromIndex;
+		int endIndex = org.length - dest.length + 1;	// endIndexは探索しない
+		int nowIndex = beginIndex;
+
+		// 探索
+		while(nowIndex < endIndex) {
+			// 後ろから探索
+			for (int i = dest.length - 1; 0 <= i; i--) {
+				byte o = org[nowIndex + i];
+				byte d = dest[i];
+				// 検証
+				if (o != d) {
+					nowIndex += skipTable[d - Byte.MIN_VALUE];
+					break;
+				}
+				// 部分配列の最後までヒットしたので発見
+				if (i == 0) {
+					return nowIndex;
+				}
+			}
+		}
+		return NOT_FOUND;
+	}
+
+	/**
+	 * 指定したデータがnull、または長さ0の時、trueを返す
+	 * @param target 検証対象
+	 * @return true:nullまたは長さ0 / false:長さ1以上
+	 */
+	private static boolean isEmpty(byte[] target) {
+		return (target == null) || (target.length < 1);
 	}
 }
--- kmkz-trial/src/test/java/jp/gr/java_conf/mahny/util/ByteUtilsTest.java
+++ kmkz-trial/src/test/java/jp/gr/java_conf/mahny/util/ByteUtilsTest.java
@@ -6,6 +6,7 @@ package jp.gr.java_conf.mahny.util;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -190,5 +191,60 @@ public class ByteUtilsTest {
 		assertThat(ByteUtils.indexOf(org, dest, 9), is(10));
 		assertThat(ByteUtils.indexOf(org, dest, 10), is(10));
 		assertThat(ByteUtils.indexOf(org, dest, 11), is(-1));
+	}
+
+	/**
+	 * 正常系
+	 * カンペいじめ
+	 * {@link jp.gr.java_conf.mahny.util.ByteUtils#indexOf(byte[], byte[], int)} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testIndexOf02_03_01() {
+		byte[] org = {
+			1, 2, 3, 4, 5, 1, 2, 1, 3, 2,
+			4, 5, 4, 3, 2, 1, 1, 2, 1, 3,
+			2, 4, 5, 9, 0, 1, 2, 3, 4, 5
+		};
+		byte[] dest = {1, 2, 1, 3, 2, 4, 5};
+
+		// 答えは、5、16
+		assertThat(ByteUtils.indexOf(org, dest, 4), is(5));
+		assertThat(ByteUtils.indexOf(org, dest, 5), is(5));
+		assertThat(ByteUtils.indexOf(org, dest, 6), is(16));
+		assertThat(ByteUtils.indexOf(org, dest, 16), is(16));
+		assertThat(ByteUtils.indexOf(org, dest, 17), is(-1));
+	}
+
+	/**
+	 * カンペ実装に対する確認用
+	 */
+	@Test
+	@Ignore("実装時の確認用なのでスキップ")
+	public void skipTableImplTest() {
+		byte[] dest = {-128, 5, 6, 4, 5, 6, 127};
+
+		int[] skipTable = new int[Byte.MAX_VALUE - Byte.MIN_VALUE + 1];
+		for (int i = 0; i < skipTable.length; i++) {
+			skipTable[i] = dest.length;
+		}
+		for (int i = 0; i < dest.length; i++) {
+			int index = dest[i] - Byte.MIN_VALUE;
+			int skip = (dest.length - (i + 1) != 0) ? dest.length - (i + 1) : 1;
+			if (skip < skipTable[index]) {
+				skipTable[index] = skip;
+			}
+		}
+
+		assertThat(skipTable[-128 - Byte.MIN_VALUE], is(6));
+		for (int i = -127; i < 4; i++) {
+			assertThat(skipTable[i - Byte.MIN_VALUE], is(dest.length));
+		}
+		assertThat(skipTable[4 - Byte.MIN_VALUE], is(3));
+		assertThat(skipTable[5 - Byte.MIN_VALUE], is(2));
+		assertThat(skipTable[6 - Byte.MIN_VALUE], is(1));
+		for (int i = 7; i < 127; i++) {
+			assertThat(skipTable[i - Byte.MIN_VALUE], is(dest.length));
+		}
+		assertThat(skipTable[127 - Byte.MIN_VALUE], is(1));
 	}
 }
